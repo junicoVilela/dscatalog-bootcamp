9d7c214c8d0bb1ba9de3707c2f798c4042ea131b
junico vilela <junico.vilela@gmail.com>
2021-07-08 17:27:16 -0300

    estudo react

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Alert from './Alert'
 
 const App = () => {
-    return <h1>Ola App</h1>
+    return (
+        <div className="container mt-5">
+            <Alert text="Junco"/>
+            <Alert text="Ana"/>
+            <Alert />
+        </div>
+    );
 }
 
 export default App;
