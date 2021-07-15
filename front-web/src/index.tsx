32468e0eedc815fcc658fb9906cb8f7f9700d66e
Valdemir Dinis Vilela Junior <valdemir@softon.com.br>
2021-07-14 21:25:33 -0300

    adicionar navbar

--- front-web/src/index.tsx
+++ front-web/src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
     <App />,
