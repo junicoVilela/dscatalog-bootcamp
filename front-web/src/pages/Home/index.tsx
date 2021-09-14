e48787eea03c8b4def71c94793644c5494ee7b14
Valdemir Dinis Vilela Junior <valdemir@softon.com.br>
2021-09-13 23:47:13 -0300

    front home

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { ReactComponent as MainImage } from '../../core/assets/images/main-image.svg';
+import './style.scss';
 
 const Home = () => (
-    <div>
-        <h1>Home</h1>
+    <div className="home-container">
+        <div className="row home-content">
+            <div className="col-6 home-text">
+                <h1 className="text-title"> Conheça o melhor <br /> catalogo de produtos</h1>
+                <p className="text-subtitle">Ajudaremoa você a encontrar os melhores <br /> produtos disponíveis no mercado.</p>
+            </div>
+            <div className="col-6">
+                <MainImage className="main-image" />
+            </div>
+
+        </div>
     </div>
 );
 
