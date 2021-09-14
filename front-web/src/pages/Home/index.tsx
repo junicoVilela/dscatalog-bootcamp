21c5b388a659a9686193d40a6a898dacd28a7b01
Valdemir Dinis Vilela Junior <valdemir@softon.com.br>
2021-09-14 20:50:37 -0300

    botao tela home

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { ReactComponent as MainImage } from '../../core/assets/images/main-image.svg';
+import ButtonIcon from '../../core/components/ButtonIcon';
+import { Link } from 'react-router-dom';
 import './style.scss';
 
 const Home = () => (
@@ -8,6 +10,9 @@ const Home = () => (
             <div className="col-6 home-text">
                 <h1 className="text-title"> Conheça o melhor <br /> catalogo de produtos</h1>
                 <p className="text-subtitle">Ajudaremoa você a encontrar os melhores <br /> produtos disponíveis no mercado.</p>
+                <Link to="/catalog">
+                    <ButtonIcon text="inicie agora a sua busca" />
+                </Link>
             </div>
             <div className="col-6">
                 <MainImage className="main-image" />
