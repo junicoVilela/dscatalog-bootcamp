901fe1013ce10b3eb94d46e773c084f50f316b1d
Valdemir Dinis Vilela Junior <valdemir@softon.com.br>
2021-09-14 21:57:06 -0300

    front listagem catalogo

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { ReactComponent as MainImage } from '../../core/assets/images/main-image.svg';
 import ButtonIcon from '../../core/components/ButtonIcon';
 import { Link } from 'react-router-dom';
-import './style.scss';
+import './styles.scss';
 
 const Home = () => (
     <div className="home-container">
-        <div className="row home-content">
+        <div className="row home-content card-base border-radius-20">
             <div className="col-6 home-text">
                 <h1 className="text-title"> Conheça o melhor <br /> catalogo de produtos</h1>
                 <p className="text-subtitle">Ajudaremoa você a encontrar os melhores <br /> produtos disponíveis no mercado.</p>
