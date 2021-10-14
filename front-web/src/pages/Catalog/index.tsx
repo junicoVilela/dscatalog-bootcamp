6c49454e25c97fe1df8d20f61f3ee68c20c14f77
Valdemir Dinis Vilela Junior <valdemir@softon.com.br>
2021-10-13 21:29:41 -0300

    desenvolvimento paginação generica

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,19 +1,21 @@
 import React, {useEffect, useState} from "react";
 import './styles.scss';
-import { makeRequest } from '../../core/utils/request'
+import { makeRequest } from 'core/utils/request'
 import ProductCard from "./components/ProductCard";
 import { Link } from 'react-router-dom';
 import { ProductsResponse } from "core/types/Product";
 import ProductCardLoader from "./components/Loaders/ProductCardLoader";
+import Pagination from "core/components/Pagination";
 
 const Catalog = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0)
 
     useEffect(() => {
         const params = {
-            page: 0,
-            linesPerPage:5
+            page: activePage,
+            linesPerPage:12
         }
 
         setIsLoading(true);
@@ -22,7 +24,7 @@ const Catalog = () => {
             .finally(() => {
                 setIsLoading(false);
             })
-    }, []);
+    }, [activePage]);
 
     return (
         <div className="catalog-container">
@@ -37,6 +39,12 @@ const Catalog = () => {
                 )}
 
             </div>
+            {productsResponse && (
+                <Pagination
+                    totalPages={productsResponse.totalPages}
+                    activePage={activePage}
+                    onChange={page => setActivePage(page)} />
+            )}
         </div>
     );
 }
