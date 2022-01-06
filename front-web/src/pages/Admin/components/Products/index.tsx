7109bdffc432e1a5d54f4a873a3006e25d7bf017
Valdemir Vilela Junior <valdemir@softon.com.br>
2022-01-06 09:33:54 -0300

    menu admin

---
+++ front-web/src/pages/Admin/components/Products/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {Route, Switch} from "react-router-dom";
+
+const Products = () => {
+    return (
+        <div>
+            <Switch>
+                <Route path="/admin/products" exact>
+                    <h1> Exibir listagem de produtos</h1>
+                </Route>
+                <Route path="/admin/products/create">
+                    <h1> Criar novo produto</h1>
+                </Route>
+                <Route path="/admin/products/:productId">
+                    <h1> Editar um produto</h1>
+                </Route>
+            </Switch>
+        </div>
+    )
+}
+
+export default Products;
