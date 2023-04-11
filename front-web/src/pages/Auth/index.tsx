8b8e44d5bebe2f3b8f155926e760669d06c24f84
valdemir vilela junior <valdemir@softon.com.br>
2023-04-11 20:53:17 -0300

    formul'ario login

---
+++ front-web/src/pages/Auth/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ReactComponent as AuthImage } from 'core/assets/images/auth.svg';
+import './styles.scss';
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+
+const Auth = () => (
+    <div className="auth-container">
+        <div className="auth-info">
+            <h1 className="auth-info-title">
+                Divulgue seus produtos <br /> no DS Catalog
+            </h1>
+            <p className="auth-info-subtitle">
+                Divulgue seus produtos no DS Catalog
+            </p>
+            <AuthImage />
+        </div>
+        <div className="auth-content">
+            <Switch>
+                <Route path="/admin/auth/login">0180061078	5B1192591	10/07/2022	271070	31/10/2022	138,42
+                    <Login />
+                </Route>
+                <Route path="/admin/auth/register">
+                    <h1>Cadastro</h1>
+                </Route>
+                <Route path="/admin/auth/recover">
+                    <h1>Recuperar Senha</h1>
+                </Route>
+            </Switch>
+        </div>
+    </div>
+);
+
+export default Auth;
