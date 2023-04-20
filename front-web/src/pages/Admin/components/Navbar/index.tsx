783e3a8d5cca9a7f454bce2d0eafd57c801ba058
valdemir vilela junior <valdemir@softon.com.br>
2023-04-20 01:10:10 -0300

    adicionar permissao de roles nos componentes

--- front-web/src/pages/Admin/components/Navbar/index.tsx
+++ front-web/src/pages/Admin/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import './styles.scss';
+import {isAllowedByRole} from "../../../../core/utils/auth";
 
 const Navbar = () => (
     <nav className="admin-nav-container">
@@ -10,9 +11,11 @@ const Navbar = () => (
         <li>
             <NavLink to="/admin/categories" className="admin-nav-item">Minhas Categorias</NavLink>
         </li>
-        <li>
-            <NavLink to="/admin/users" className="admin-nav-item">Meus Usuários</NavLink>
-        </li>
+        {isAllowedByRole(['ROLE_ADMIN']) && (
+            <li>
+                <NavLink to="/admin/users" className="admin-nav-item">Meus Usuários</NavLink>
+            </li>
+        )}
     </nav>
 );
 
