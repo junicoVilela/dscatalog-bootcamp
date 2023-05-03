58919200be67ab7288d76fbacfd55f485d11444f
valdemir vilela junior <valdemir@softon.com.br>
2023-05-03 01:29:35 -0300

    correção nome botão

--- front-web/src/pages/Admin/components/BaseForm/index.tsx
+++ front-web/src/pages/Admin/components/BaseForm/index.tsx
@@ -20,7 +20,7 @@ const BaseForm = ({ title, children }: Props) => {
           {children}
         <div className="base-form-actions">
             <button className="btn btn-outline-danger border-radius-10 me-3" onClick={handleCancel}>CANCELAR</button>
-            <button className="btn btn-primary border-radius-10">CADASTRAR</button>
+            <button className="btn btn-primary border-radius-10">SALVAR</button>
         </div>
       </div>
   )
