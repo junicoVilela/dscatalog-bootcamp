4bf627ba406a4afe5a83e3855d611fa337c44990
valdemir vilela junior <valdemir@softon.com.br>
2023-05-04 23:43:03 -0300

    adicionando loader lista de produto

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -5,6 +5,7 @@ import { ProductsResponse } from "../../../../../core/types/Product";
 import Pagination from "core/components/Pagination";
 import { makePrivateRequest, makeRequest } from "core/utils/request";
 import { toast } from "react-toastify";
+import CardLoader from "../Loaders/ProductCardLoader";
 
 const List = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
@@ -57,10 +58,11 @@ const List = () => {
               ADICIONAR
           </button>
           <div className="admin-list-container">
-              <h1>Listagem de produtos</h1>
-              {productsResponse?.content.map(product => (
-                  <Card product={product} key={product.id} onRemove={onRemove} />
-              ))}
+              {isLoading ? <CardLoader /> : (
+                  productsResponse?.content.map(product => (
+                    <Card product={product} key={product.id} onRemove={onRemove} />
+                  ))
+              )}
               {productsResponse && (
               <Pagination
                 totalPages={productsResponse.totalPages}
